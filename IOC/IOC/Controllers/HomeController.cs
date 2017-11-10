ba5a11e1b8039a3d4035ab6ad577c9b3ef34d104
gebizhuifengren <gebizhuifengren@aliyun.com>
2017-11-10 18:31:53 +0800

    ioc

---
+++ IOC/IOC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IOC.Models;
+using IOC.Model;
+
+namespace IOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person person = null;
+        public HomeController(Person person)
+        {
+            this.person = person;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return Content(person.GetName());
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IOC/IOC/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOC.Model
+{
+    public class Person
+    {
+        public string GetName()
+        {
+            return "zhangsan";
+        }
+    }
+}
